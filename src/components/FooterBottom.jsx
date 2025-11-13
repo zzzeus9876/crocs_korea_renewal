2036d5478789a2db392e5f3a56980837c902eed6
Lim Chae A <imchae4@gmail.com>
2025-11-13 13:11:25 +0900

    헤더 푸터 수정/ 푸터 scss 합침

--- src/components/FooterBottom.jsx
+++ src/components/FooterBottom.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import './scss/FooterBottom.scss';
 import { Link } from 'react-router-dom';
 
 const FooterBottom = () => {
     return (
         <>
             <div className="footer_bottom">
-                <Link to="/cscenter">CS Center</Link>{' '}
-                <Link to="/companyinfo">
-                    <span>INFO</span>
-                </Link>
+                <div className="wide_inner">
+                    <Link to="/cscenter">CS Center</Link>{' '}
+                    <Link to="/companyinfo">
+                        <span>INFO</span>
+                    </Link>
+                </div>
             </div>
         </>
     );
--- src/components/FooterContents.jsx
+++ src/components/FooterContents.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './scss/FooterContent.scss';
 
 const FooterContents = () => {
     return (
