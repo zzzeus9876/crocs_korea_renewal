7958fb08d229ef0d301e7ac3212b695046cc4b90
chowon <zzzeus9876@gmail.com>
2025-11-29 23:36:45 +0900

    2025-11-29(토) 초원 env 파일 firebase 변수 적용

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,12 +8,12 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: 'AIzaSyDVZl8jVtpYocsHB8wr5ep-6Z7smuj0dYc',
-    authDomain: 'crocs-korea-renewal.firebaseapp.com',
-    projectId: 'crocs-korea-renewal',
-    storageBucket: 'crocs-korea-renewal.firebasestorage.app',
-    messagingSenderId: '730202810243',
-    appId: '1:730202810243:web:e9856329d6b6cc3fed78b6',
+    apiKey: process.env.REACT_APP_FIREBASER_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASER_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASER_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASER_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASER_APP_ID,
 };
 
 // Initialize Firebase
