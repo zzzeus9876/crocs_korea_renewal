cc10679c502edae42fe3308e4ddc9ac616bd7038
jiin <amesystand@gmail.com>
2025-12-01 03:38:27 +0900

    Revert "2025-12-01(월) 지인 컬러필터 사이즈필터 전체제거버튼 추

--- src/components/LeftNavigation.jsx
+++ src/components/LeftNavigation.jsx
@@ -3,21 +3,25 @@ import Breadcrumbs from "./Breadcrumbs";
 import SizeMenu from "./SizeMenu";
 import FilterMenu from "./FilterMenu";
 import ColorMenu from "./ColorMenu";
-import { useCrocsSizeStore } from "../store/useCrocsSizeStore";
-import { useColorFilterStore } from "../store/useColorFilterStore";
-import { useParams } from "react-router-dom";
 import "./scss/leftNavigation.scss";
+import { useCrocsSizeStore } from "../store/useCrocsSizeStore";
+import { useParams } from "react-router-dom";
+import { useColorFilterStore } from "../store/useColorFilterStore";
 
 export default function LeftNavigation({
   category,
   subcategory,
   selectedSize,
   onSizeSelect,
+  filters = [],
+  priceRanges = [],
+  colors = [],
 }) {
   const { crocsSizes, onFetchSize } = useCrocsSizeStore();
   const { selectedColors, toggleColor } = useColorFilterStore();
   const params = useParams();
 
+  // ⭐ 최종적으로 사용할 category, subcategory
   const finalCategory = category || params.cate || "new";
   const finalSubcategory = subcategory || params.subcategory || null;
 
@@ -26,24 +30,26 @@ export default function LeftNavigation({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const handleRemoveColor = (color) => toggleColor(color);
-  const handleRemoveSize = () => onSizeSelect?.(null);
+  // 색상 제거 핸들러
+  const handleRemoveColor = (colorToRemove) => {
+    toggleColor(colorToRemove);
+  };
+
+  // 사이즈 제거 핸들러
+  const handleRemoveSize = () => {
+    onSizeSelect?.(null);
+  };
 
   return (
     <div className="left_nav__section_wrap">
       <Breadcrumbs category={finalCategory} subcategory={finalSubcategory} />
       <div className="left_nav">
+        {/* ⭐ 선택된 필터들을 FilterMenu에 전달 */}
         <FilterMenu
           selectedColors={selectedColors}
           selectedSize={selectedSize}
           onRemoveColor={handleRemoveColor}
           onRemoveSize={handleRemoveSize}
-          onRemoveAll={() => {
-            // 모든 색상 초기화
-            selectedColors.forEach((color) => toggleColor(color));
-            // 사이즈 초기화
-            onSizeSelect?.(null);
-          }}
         />
 
         <div className="breadcrumbs__line" />
@@ -57,7 +63,10 @@ export default function LeftNavigation({
         <div className="breadcrumbs__line" />
 
         <ColorMenu />
+
         <div className="breadcrumbs__line" />
+
+        {/* <PriceMenu priceRanges={priceRanges} /> */}
       </div>
     </div>
   );
