28f109490cc0bdf468461f973923efbd5ebfc121
amesystand <amesystand@gmail.com>
2025-12-01 11:09:15 +0900

    2025-12-01(월) 푸터 sns 추가

--- src/components/FooterContents.jsx
+++ src/components/FooterContents.jsx
@@ -18,16 +18,40 @@ const FooterContents = () => {
             <div className="footer_right">
                 <div className="sns_img_box">
                     <p>
-                        <img src="/images/icon-insta.png" alt="" />
+                        <a
+                            href="https://www.instagram.com/crocskorea/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img src="/images/icon-insta.png" alt="icon-insta" />
+                        </a>
                     </p>
                     <p>
-                        <img src="/images/icon-youtube.png" alt="" />
+                        <a
+                            href="https://www.youtube.com/channel/UCIYfU7V73aPdvTlRzDK5tcg"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img src="/images/icon-youtube.png" alt="icon-youtube" />
+                        </a>
                     </p>
                     <p>
-                        <img src="/images/icon-facebook.png" alt="" />
+                        <a
+                            href="https://pf.kakao.com/_zxhxkpd"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img src="/images/icon-kakao.png" alt="icon-kakao" />
+                        </a>
                     </p>
                     <p>
-                        <img src="/images/icon-earth.png" alt="" />
+                        <a
+                            href="https://www.tiktok.com/tag/%ED%81%AC%EB%A1%9D%EC%8A%A4"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img src="/images/icon-tiktok.png" alt="icon-tiktok" />
+                        </a>
                     </p>
                 </div>
                 <div className="copy-text">© 2025 Crocs Retail, LLC</div>
